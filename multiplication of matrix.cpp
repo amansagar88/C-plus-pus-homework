d520cf0d0707caee44df7aca5327704e6219f8ae
Aman_Sagar <90393733+amansagar88@users.noreply.github.com>
2022-12-27 17:51:33 +0530

    Create multiplication of matrix.cpp

---
+++ multiplication of matrix.cpp
@@ -0,0 +1,42 @@
+//multiplication of two square matrix
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r, c, i, j, a[100][100], b[100][100], d[100][100];
+    cout<<"Enter the number of row you want in the matix: ";
+    cin>>r;
+    cout<<"Enter the number of column you want in the matix: ";
+    cin>>c;
+    for (i = 0 ; i < r; ++i){
+        for(j = 0; j < c; ++j){
+            cout<<"Enter the value of a"<<i+1<<j+1<<" : ";
+            cin>>a[i][j];
+        }
+        cout<<endl;
+    }
+
+    for (i = 0 ; i < r; ++i){
+        for(j = 0; j < c; ++j){
+            cout<<"Enter the value of b"<<i+1<<j+1<<" : ";
+            cin>>b[i][j];
+        }
+        cout<<endl;
+    }
+
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            d[i][j] = 0;
+            for (int k = 0; k < r; k++) {
+                d[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    for (i = 0 ; i < r; ++i){
+        for(j = 0; j < c; ++j){
+            cout<<d[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
